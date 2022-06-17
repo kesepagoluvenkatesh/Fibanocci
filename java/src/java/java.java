e84a7ccde0511f9aeb0673811d2eac66ebb46fc5
ramadevi <ramadevi@deviK-T017>
2022-06-17 15:29:35 +0530

    commit

---
+++ java/src/java/java.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+package venky;
+
+public class java {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.print("even");
+		}
+			else
+				System.out.print("odd");	
+		// TODO Auto-generated method stub
+
+	}
+
+}
